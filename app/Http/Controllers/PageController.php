29167a92c040a370a061fca661c9895803c9e221
jamshidrajabov <jamshidamaliymatematika@gmail.com>
2024-02-21 22:43:49 +0500

    21.02.2024 22:43

---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PageController extends Controller
+{
+    public function main()
+    {
+        return view('main');
+    }
+    public function about()
+    {
+        return view('about');
+    }
+    public function services()
+    {
+        return view('services');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PageController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+/*Route::get('/', function () {
     return view('welcome');
-});
+});*/
+Route::get('/',[PageController::class,'main']);
+Route::get('about',[PageController::class,'about'])->name('about');
+Route::get('services',[PageController::class,'services'])->name('services');
