b5073b63b48c2a025fdd2479a2cae8e1333c24d0
jamshidrajabov <jamshidamaliymatematika@gmail.com>
2024-02-24 12:01:06 +0500

    post chiqarish va post korish ishlandi

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
@@ -10,8 +11,26 @@ class PostController extends Controller
      * Display a listing of the resource.
      */
     public function index()
-    {
-        return view('posts.index');
+    {/*
+     $post = Post::create([
+        'title' => 'London to Paris',
+        'short_content'=>'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit, odit consequuntur. Architecto neque, similique dignissimos nesciunt consectetur voluptatum deserunt esse.',
+        'content'=>' Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae molestias, 
+        dolor expedita dolore illum, assumenda numquam repellendus tenetur hic 
+        veritatis similique est eos suscipit incidunt sunt repudiandae nobis, 
+        maiores corrupti tempora esse nesciunt ipsum. Iusto ab voluptatem, eaque 
+        corrupti totam officiis? Excepturi error alias tempore ipsam inventore 
+        corrupti est, possimus corporis saepe quia sapiente fugit, minus praesentium 
+        vitae laboriosam ad voluptate? Nostrum, vero aliquid harum quasi consequatur 
+        quod praesentium delectus rerum tempore suscipit voluptatibus vitae autem. 
+        Earum corporis officia provident nam qui mollitia, sapiente esse tempore 
+        soluta obcaecati animi necessitatibus, fugiat numquam suscipit, dicta 
+        non sequi explicabo quam. Nisi, velit!',
+        'photo'=>'storage/545.png'
+       
+    ]);*/
+   $posts = Post::all();    
+    return view('posts.index',['posts'=>$posts]);
     }
 
     /**
@@ -35,7 +54,13 @@ class PostController extends Controller
      */
     public function show(string $id)
     {
-        //
+        $recent_posts=Post::latest()->get()->except($id)->take(5);
+        $post=Post::find($id);
+        return view('posts.show',[
+            'post'=>$post,
+            'recent_posts'=>$recent_posts,
+      
+        ]);
     }
 
     /**
